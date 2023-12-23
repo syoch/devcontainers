02c0cb8715e57789103efe2afc17bf153e654092
syoch <syoch64@gmail.com>
2023-12-23 13:52:16 +0000

    Update: Fix Esp-idf install user

--- src/esp-idf/install.sh
+++ src/esp-idf/install.sh
@@ -8,6 +8,6 @@ rm -rf /var/lib/apt/lists/*
 
 mkdir /opt/esp-idf
 git clone --recursive https://github.com/espressif/esp-idf.git /opt/esp-idf -b $TARGET
-/opt/esp-idf/install.sh esp32 --enable-gdbgui
+sudo -u vscode /opt/esp-idf/install.sh esp32 --enable-gdbgui
 
 rm -rf ~/.espressif
