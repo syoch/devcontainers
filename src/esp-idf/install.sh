0931463f46a888667b53b7f3973b2f415ef92f8c
syoch <syoch64@gmail.com>
2023-12-23 10:57:26 +0000

    Update: fix some

--- src/esp-idf/install.sh
+++ src/esp-idf/install.sh
@@ -3,12 +3,11 @@
 set -e
 
 apt update
-apt install python3 python3-venv
+apt install -y python3 python3-venv
 rm -rf /var/lib/apt/lists/*
 
 mkdir /opt/esp-idf
-git clone --recursive https://github.com/espressif/esp-idf.git /opt/esp-idf
+git clone --recursive https://github.com/espressif/esp-idf.git /opt/esp-idf -b $TARGET
 /opt/esp-idf/install.sh esp32 --enable-gdbgui
-python3 /opt/esp/esp-idf/tools/idf_tools.py install qemu-xtensa
 
 rm -rf ~/.espressif
