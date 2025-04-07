f069896b7eba0a5062fa848729d71f6e30a2458b
syoch <syoch64@gmail.com>
2025-04-07 23:56:09 +0900

    Update: Prebuilt container

--- robotics/container/scripts/libs_mbed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf "\e[32mInstallin libs-cmake\e[0m\n"
-git clone git@github.com:nnctroboticsclub/libs-cmake.git /tmp/libs-cmake
-make -C /tmp/libs-cmake install
-
-printf "\e[32mInstallin static-mbed-os\e[0m\n"
-git clone git@github.com:nnctroboticsclub/static-mbed-os.git /tmp/static-mbed-os
-make -C /tmp/static-mbed-os install TARGET=NUCLEO_F446RE
--- robotics/container/scripts/setup_node.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-printf "\e[32mSetting up Node.js\e[0m\n"
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-. ~/.nvm/nvm.sh
-
-nvm install 20
